826cd3a8eb7793e4b36fc2d8d1bbb83b1d4db242
Gustavo Sudre <gustavo.sudre@nih.gov>
2017-10-03 16:46:01 -0400

    first try on refinement pipeline

---
+++ gatk_refine.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#SBATCH --job-name=gatk1
+###SBATCH --mail-type=ALL
+#BROAD Best Practice
+
+#####################
+#Parameters to check#
+#####################
+id=$1
+gatk_memory="50g"
+ref_fa="/fdb/igenomes/Homo_sapiens/UCSC/hg19/Sequence/WholeGenomeFasta/genome.fa"
+hg19_known_snps="/fdb/GATK_resource_bundle/b37/1000G_phase3_v4_20130502.sites.vcf.gz"
+home_directory="/data/NCR_SBRB/big_fake_simplex"
+vcf_directory="${home_directory}/VCF"
+my_gc_threads=$(($SLURM_CPUS_PER_TASK-1))
+
+################
+#Upload modules#
+################
+module load GATK/3.6
+
+trio_fname=${home_directory}/${1}.ped
+trio_name=$1
+
+cd $vcf_directory
+# Step 1: Derive posterior probabilities of genotypes
+GATK -m ${gatk_memory} CalculateGenotypePosteriors -R ${ref_fa} --supporting ${hg19_known_snps} -ped ${trio_fname} -pedValidationType SILENT -V recalibrated_variants.vcf -o recalibrated_variants.${trio_name}.vcf 
+
+# Step 2: Filter low quality genotypes
+GATK -m ${gatk_memory} VariantFiltration -R ${ref_fa} -V recalibrated_variants.${trio_name}.vcf -G_filter "GQ < 20.0" -G_filterName lowGQ -o recalibrated_variants.${trio_name}.Gfiltered.vcf
+
+# Step 3: Annotate possible de novo mutations
+GATK -m ${gatk_memory} VariantAnnotator -R ${ref_fa} -V recalibrated_variants.${trio_name}.Gfiltered.vcf -A PossibleDeNovo -ped ${trio_fname} -o recalibrated_variants.${trio_fname}.Gfiltered.deNovos.vcf
